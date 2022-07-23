3e73976cbe07072e79a4ab1df0eb38e9006af545
YJ Guo <1119272440@qq.com>
2022-07-23 08:59:40 +0800

    pro1 finished

--- pro/pro1/ColorMapInput.c
+++ pro/pro1/ColorMapInput.c
@@ -18,6 +18,19 @@
 uint8_t** FileToColorMap(char* colorfile, int* colorcount)
 {
 	//YOUR CODE HERE
+	FILE* color_file = fopen(colorfile, "r");
+	if(color_file == NULL){
+		fclose(color_file);
+		return NULL;
+	}
+	fscanf(color_file, "%d", colorcount);
+	uint8_t** array = (uint8_t**)malloc(*colorcount*sizeof(uint8_t*));
+	for(int i = 0; i < *colorcount; i++){
+		array[i] = (uint8_t*)malloc(3*sizeof(uint8_t));
+		fscanf(color_file, "%d %d %d", &array[i][0], &array[i][1], &array[i][2]);
+	}
+	fclose(color_file);
+	return array;
 }
 
 
