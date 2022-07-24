8fc0fb2c41a4efa5e6689a6d96537cbaafb41445
YJ Guo <1119272440@qq.com>
2022-07-24 21:29:04 +0800

    lab03 finished

--- lab/lab03/bit_ops.c
+++ lab/lab03/bit_ops.c
@@ -9,7 +9,9 @@ unsigned get_bit(unsigned x,
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns 
     // 0 or 1)
-    return -1;
+    unsigned temp = 1 << n;
+    x &= temp;
+    return x >> n;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
@@ -17,11 +19,38 @@ void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
     // YOUR CODE HERE
+    // if(0 == v){
+    //     unsigned temp = ((0XFFFFFFFF << (n+1)) | (0XFFFFFFFF >> (32-n)));
+    //     (*x) &= temp;
+    // }
+    // else
+    //     *x |= (1 << n);
+    	unsigned a = 1;
+	unsigned b = 0;
+	
+	b = ~b; 
+	b = b<<(n+1);
+	
+	// 构造 v; v = 1x11, x是要改变成的值 0 or 1.
+	v = ~v;
+	v = v << n;
+	v = ~v;
+	v = v|b;
+	
+	// 把 x 的 n 位置为 1
+	a = a << n;
+	(*x) = (*x)|a;
+	
+	(*x) = (*x)&v;
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
     // YOUR CODE HERE
+    if(get_bit(*x, n))
+        set_bit(x, n, 0);
+    else
+        set_bit(x, n, 1);
 }
 
--- lab/lab03/lfsr.c
+++ lab/lab03/lfsr.c
@@ -6,5 +6,13 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t reg_ = *reg;
+    uint16_t temp = 0;
+    temp ^= reg_&0X0001;
+    temp ^= (reg_>>2)&0X0001;
+    temp ^= (reg_>>3)&0X0001;
+    temp ^= (reg_>>5)&0X0001;
+    temp = temp << 15;
+    *reg = temp | (*reg >> 1);
 }
 
